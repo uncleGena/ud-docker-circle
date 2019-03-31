0411922bc9c2b8def2f8ae9264440f74d908216c
Kudriavtsev Andrew <mail.andrew.k@gmail.com>
2019-03-31 11:33:06 +0300

    code changed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> AND dd reload.TWENTY
+            bla balalallaallsdkf a;sdfl kjasd;fl kjasdf;l
           </p>
           <a
             className="App-link"
